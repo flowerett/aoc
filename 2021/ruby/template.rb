284ba2ba541edd0fcf0c5232832684e2e90c2cf8
Nick C <nick.chernyshev@gmail.com>
2021-12-12 16:43:55 +0100

    2021: add ruby template

---
+++ 2021/ruby/template.rb
@@ -0,0 +1,26 @@
+#! /usr/bin/env ruby
+# require "set"
+require_relative "nanotest"
+
+input = <<INPUT
+test
+INPUT
+
+# input = File.read("../inputs/day1x")
+
+VERBOSE = ARGV.delete("-v")
+
+def prep_data(input)
+  input.strip.split("\n").map { |r| r }
+end
+
+data = prep_data(input)
+pp data
+
+# if $0 == __FILE__
+#   Utils.day(2021, x)
+#   t = NanoTest.new("test")
+#   t.assert_all(tr, sol, "example 1")
+
+#   Utils.pp([r1, r2])
+# end
