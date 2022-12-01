b9c22df7588244b03dbc24ef9248fc422404615a
Nick C <nick.chernyshev@gmail.com>
2022-12-01 06:32:49 +0100

    2022: py day1

--- 2022/python/day1.py
+++ 2022/python/day1.py
@@ -6,15 +6,35 @@ import minitest
 
 
 def solve(data):
-    return [int(x) for x in data]
+    sums = []
+    for ss in data:
+        dd = [int(x) for x in ss.split('\n')]
+
+        psum = sum(dd)
+        sums.append(psum)
+
+    r1 = max(sums)
+
+    r2 = sum(sorted(sums, reverse=True)[:3])
+
+    return r1, r2
 
 
 if __name__ == '__main__':
     TEST_INP = '../inputs/day1t'
+    LIVE_INP = '../inputs/day1'
+    TEST_RES = (24000, 45000)
+    DAY1_RES = (70369, 203002)
 
     with open(TEST_INP) as f:
-        data = f.read().strip().split('\n')
+        data = f.read().strip().split('\n\n')
+        minitest.assert_all(solve(data), TEST_RES, 'TEST_INP')
 
-        r = solve(data)
+    with open(LIVE_INP) as f:
+        data = f.read().strip().split('\n\n')
 
-        print('res: ', r)
+        r1, r2 = solve(data)
+        minitest.assert_all((r1, r2), DAY1_RES, 'LIVE_INP')
+
+        print('res1: ', r1)
+        print('res2: ', r2)
